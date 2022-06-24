c1f7ff9954e769abc044eb1dce6d904b54b0833a
Shivang Garg <shivang10garg@gmail.com>
2022-06-24 22:30:43 +0530

    allows users to be added to group and view particular group details

--- app.py
+++ app.py
@@ -16,10 +16,17 @@ class App:
         new_group_id = IdGenerate().id_generated
         self.groups[new_group_id] = Group(name, desc, new_group_id)
 
+    def add_user_to_group(self, group_id, current_new_users):
+        self.groups[group_id].add_users(current_new_users)
+
     def list_users(self):
         for user in self.users:
             print(self.users[user].first_name, self.users[user].phone_no)
 
     def list_groups(self):
         for group in self.groups:
-            print(self.groups[group].name, self.groups[group].desc)
+            print(self.groups[group].name, self.groups[group].desc, self.groups[group.group_id])
+
+    def get_group_details(self, group_id):
+        for user in self.groups[group_id]:
+            print(user.first_name, user.last_name, user.user_id)
